6687370c3103af7cf9b8372e6e603446516f5bfe
mudjakils <20191554@nbsc.edu.ph>
2024-04-29 18:37:03 +0800

    added changes

--- db/config.php
+++ db/config.php
@@ -2,9 +2,10 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'madrijanon-pdo-crud');
+define('DB_USERNAME', 'u593341949_dev_madrijanon');
+define('DB_NAME', 'u593341949_db_madrijanon');
+define('DB_PASSWORD', '20191554Madrijanon');
+
  
 /* Attempt to connect to MySQL database */
 try{
